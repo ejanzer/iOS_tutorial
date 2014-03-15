2569e4f739a7e387142c87ac2a69c2d5bc2b20ac
Emily Janzer <emily.janzer@gmail.com>
2014-03-15 13:46:27 -0700

    Initial Commit

---
+++ ToDoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by Emily Janzer on 3/15/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
